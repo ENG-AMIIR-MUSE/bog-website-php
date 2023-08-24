7891ba44ca652fd8aae627eb26f3b4ae9d70fa7a
AmirMuse <amirlearn08@gmail.com>
2023-08-23 18:35:44 -0700

    SetUp the porject frontend

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+include('./includes/header.php');
+include('./includes/top-nav.php');
+include('./includes/sidebar.php')
+?>
+
+<div class="container">
+    <h1>welcome</h1>
+</div>
+<?php
+include('./includes/footer.php');
+include('./includes/scripts.php')
+?>
